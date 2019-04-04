8ddd8c3049f9bd2a9d71b1ccf083206d18058e32
Tony <kloddant@users.noreply.github.com>
2019-04-04 13:44:49 -0500

    Separated the keys and values in the kwargs

--- decorators.py
+++ decorators.py
@@ -4,17 +4,19 @@ from hashlib import md5
 def cache_method(method, timeout=3600):
 
 	def wrapper(*args, **kwargs):
-
+		
 		key = md5(repr(method).encode('utf-8'))
 		for arg in args:
 			key.update(repr(arg).encode('utf-8'))
-		for kwarg in kwargs:
-			key.update(repr(kwarg).encode('utf-8'))
+		for kwarg_key, kwarg_value in kwargs.items():
+			key.update(repr(kwarg_key).encode('utf-8'))
+			key.update(repr(kwarg_value).encode('utf-8'))
 		key = key.hexdigest()
 		result = cache.get(key)
 		if not result:
 			result = method(*args, **kwargs)
 			cache.set(key, result, timeout)
+
 		return result
 
 	return wrapper
